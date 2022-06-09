5ba83d5724ee1f825f84a9c2f740d0ac0dac79ff
Attapinya Mittarawong <kudo@MacBook-Pro-khxng-Attapinya.local>
2022-06-09 14:43:20 +0700

    edit database.php

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$DATABASE = mysqli_connect("localhost", "root", "", "id18953963_kku_db");
+$DATABASE = mysqli_connect("localhost", "admin", "H&^fg567Hk", "check_borrow_db");
 mysqli_set_charset($DATABASE, "utf8");
 if ($DATABASE->connect_error) {
     echo die("Connection failed: " . $DATABASE->connect_error);
